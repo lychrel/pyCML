1525d4d0ac53a4c134101dbcaecac11502464141
Jack Lynch <jacklynchtds@gmail.com>
2018-12-25 22:53:15 -0500

    Removed arbitrated comment from demo

--- demo.py
+++ demo.py
@@ -3,11 +3,9 @@ from maps import KanekoLogistic
 from couplings import TwoNeighbor
 from evolution import Evolution
 
-"""
-BUG: doesn't work
-"""
-
-cml = CML(dim=100, coupling=TwoNeighbor(strength=0.2, map_obj=KanekoLogistic(alpha=1.47)))
+cml = CML(dim=100,
+          coupling=TwoNeighbor(strength=0.2,
+                               map_obj=KanekoLogistic(alpha=1.47)))
 
 ev = Evolution(cml)
 ev.plot_time_evolution(10)
